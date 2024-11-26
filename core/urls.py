6ae555cbf5fa5c434384afcb9bb8ac28ccad9337
polumna <polumna13umida@gmail.com>
2024-11-26 18:32:54 +0500

    initial

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from main.views import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view()),
